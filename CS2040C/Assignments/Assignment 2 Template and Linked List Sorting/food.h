8440fe839b37df70fd1ae2962610eb9ee726746d
LAPTOP-6AQ96QAR\richwill <richwill2812@gmail.com>
2021-10-06 14:52:34 +0800

    Remove tabs

--- CS2040C/Assignments/Assignment 2 Template and Linked List Sorting/food.h
+++ CS2040C/Assignments/Assignment 2 Template and Linked List Sorting/food.h
@@ -3,19 +3,19 @@
 using namespace std;
 
 class Food {
-	private:
-		string _name;
-		int _cal;
+    private:
+        string _name;
+        int _cal;
 
 	public:
-		Food() { _name = ""; _cal = 0; };
-		Food(string, int);
-		Food operator+(const Food&);
-		bool operator>(const Food&);
-		bool operator==(const Food&);
-		string name() { return _name; };
-		int cal() { return _cal; };
-		friend ostream &operator<<(ostream&, const Food&);
+        Food() { _name = ""; _cal = 0; };
+        Food(string, int);
+        Food operator+(const Food&);
+        bool operator>(const Food&);
+        bool operator==(const Food&);
+        string name() { return _name; };
+        int cal() { return _cal; };
+        friend ostream &operator<<(ostream&, const Food&);
 };
 
 #include "food.cpp"
--- CS2040C/Assignments/Assignment 2 Template and Linked List Sorting/simpleLinkedListTemplate.h
+++ CS2040C/Assignments/Assignment 2 Template and Linked List Sorting/simpleLinkedListTemplate.h
@@ -6,38 +6,38 @@ class List;
 
 template <class T>
 class ListNode {
-	private:
-		T _item;
-		ListNode<T> *_next;
+    private:
+        T _item;
+        ListNode<T> *_next;
 
-	public:
-		ListNode(T);
-		T content() { return _item; };
-		void print() { cout << _item; };
-		friend class List<T>;
+    public:
+        ListNode(T);
+        T content() { return _item; };
+        void print() { cout << _item; };
+        friend class List<T>;
 };
 
 template <class T>
 class List {
-	private:
-		int _size;
-		ListNode<T> *_head;
+    private:
+        int _size;
+        ListNode<T> *_head;
 
-	public:
-		// for the following functions, you cannot assume that the list is not empty or empty...
-		List()	{	_size = 0;	_head = NULL; };
-		~List();
-		void insertHead(T);
-		void print(bool withNL = false);    // print the items in one single row if false, otherwise, print each item in a new line
-		int size() { return _size; };
-		bool exist(T);
-		bool empty() { return _size == 0; };
-		void reverseOp();
+    public:
+        // for the following functions, you cannot assume that the list is not empty or empty...
+    List()	{	_size = 0;	_head = NULL; };
+        ~List();
+        void insertHead(T);
+        void print(bool withNL = false);    // print the items in one single row if false, otherwise, print each item in a new line
+        int size() { return _size; };
+        bool exist(T);
+        bool empty() { return _size == 0; };
+        void reverseOp();
 
-		// .. except for the following functions, we can assume that the list is not empty when you call them.
-		T headItem();
-		T extractMax();
-		void removeHead();
+        // .. except for the following functions, we can assume that the list is not empty when you call them.
+        T headItem();
+        T extractMax();
+        void removeHead();
 };
 
 #include "simpleLinkedListTemplate.cpp"
