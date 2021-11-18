3c26b35c34bcd17aa21026edef829722ef1ae301
richwill28 <e0550368@u.nus.edu>
2021-11-18 11:53:37 +0800

    Feat(CS2040C): Add necessary DS for assignment 5

---
+++ CS2040C/Assignments/Assignment 5 SSSP/simpleLinkedListTemplate.h
@@ -0,0 +1,51 @@
+#pragma once
+#include <iostream>
+using namespace std;
+
+template <class T>
+class List;
+
+template <class T>
+class ListNode {
+    private:
+        T _item;
+        ListNode<T> *_next;
+
+    public:
+        ListNode(T);
+        T content() { return _item; }
+        void print() { cout << _item; }
+        friend class List<T>;
+};
+
+template <class T>
+class List {
+    private:
+        int _size;
+        ListNode<T> *_head;
+        ListNode<T> *_current;
+
+    public:
+        // for the following functions, you cannot assume that the list is not empty or empty...
+        List() { _size = 0; _head = NULL; _current = NULL; }
+        ~List();
+        void insertHead(T);
+        void print(bool withNL = false);    // print the items in one single row if false, otherwise, print each item in a new line
+        int size() { return _size; }
+        bool exist(T);
+        bool empty() { return _size == 0; }
+        void reverseOp();
+
+        // ...except for the following functions, we can assume that the list is not empty when you call them
+        T headItem();
+        T extractMax();
+        void removeHead();
+
+        // simple iterator
+        void start() { _current = _head; }
+        T current() { if (_current) return _current->_item; }
+        void next() { if (_current) _current = _current->_next; }
+        bool end() { return _current == NULL; }
+};
+
+#include "simpleLinkedListTemplate.cpp"
